e57ae3181ef39d1c73305b2f95bdd94a3295ee04
pbaranski <pbaranski@future-processing.com>
2014-05-22 01:31:13 +0200

    Changed foreach, added 'Filesystem' handling and other improvements  issue #3

--- oop/disc_connector.py
+++ oop/disc_connector.py
@@ -7,14 +7,11 @@ class DiskConnector(object):
     def __init__(self, console_query):
         self._disks = []
         output = console_query.out()
-        output_lines = output.stdout.readlines()
-        for line in output_lines:
-            line = line.split()
-            disk = Disk()
-            disk.name = line[0]
-            disk.size = line[1]
-            disk.free_space = line[3]
-            self._disks.append(disk)
+        for line in output.stdout:
+            if line.startswith('Filesystem'):
+                continue
+            name, size, used, free, used_percent, location = line.split()
+            self._disks.append(Disk(name=name, size=size, free_space=free))
 
     def disks_count(self):
         return len(self._disks)
