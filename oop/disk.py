4d969e9669728b7c92495f66758a32031640698e
pbaranski <pbaranski@future-processing.com>
2014-05-22 01:21:41 +0200

    Changed disk class behaviour issue #3

--- oop/disk.py
+++ oop/disk.py
@@ -2,10 +2,10 @@ __author__ = 'szemek'
 
 
 class Disk(object):
-    def __init__(self):
-        self.name = ''
-        self.size = 0
-        self.free_space = 0
+    def __init__(self, name='', size=0, free_space=0):
+        self.name = name
+        self.size = size
+        self.free_space = free_space
 
     def show(self):
         return 'name:', self.name, ' size:', self.size, ' free:', self.free_space
